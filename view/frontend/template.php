928595f1701125c8abdc40b0a98f03bd9f698aad
CyrilHumbert <cyril77139@msn.com>
2018-01-29 23:27:32 +0100

    Modification du template, link vers les fichiers css bootstrap et jquery

--- view/frontend/template.php
+++ view/frontend/template.php
@@ -3,10 +3,13 @@
     <head>
         <meta charset="utf-8" />
         <title><?= $title ?></title>
-        <link href="public/css/style.css" rel="stylesheet" /> 
+        <link href="public/css/bootstrap.min.css" rel="stylesheet"> 
     </head>
         
     <body>
         <?= $content ?>
+
+        <script src="public/js/jquery-3.3.1.min.js"></script>
+        <script src="public/js/bootstrap.min.js"></script>
     </body>
 </html>
